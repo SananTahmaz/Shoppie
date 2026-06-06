a35b90f206acbc4b2a4cd44a3a7497ede534b605
Sanan Tahmazli <sanan.tahmazli2329@gmail.com>
2026-06-06 19:23:01 +0400

    Added OpenApi dependency and created Swagger UI

---
+++ src/main/java/com/shoppie/configurations/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package com.shoppie.configurations;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Shoppie")
+                                .version("1.0")
+                                .description("REST API documentation for Shoppie")
+                );
+    }
+
+    @Bean
+    public GroupedOpenApi userApi() {
+        return GroupedOpenApi
+                .builder()
+                .group("users")
+                .pathsToMatch("/api/users/**")
+                .build();
+    }
+}
