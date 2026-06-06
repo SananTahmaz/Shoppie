16c09995a6f4d8d5307ff275162803e49c3b3f1d
Sanan Tahmazli <sanan.tahmazli2329@gmail.com>
2026-06-06 19:55:03 +0400

    Formatted lines

--- src/main/java/com/shoppie/services/UserService.java
+++ src/main/java/com/shoppie/services/UserService.java
@@ -8,10 +8,16 @@ import java.util.List;
 
 public interface UserService {
     UserResponse register(UserRegisterRequest request);
+
     UserResponse getById(Long id);
+
     List<UserResponse> getAll();
+
     UserResponse update(Long id, UserUpdateRequest request);
+
     void delete(Long id);
+
     UserResponse freeze(Long id);
+
     UserResponse activate(Long id);
 }
