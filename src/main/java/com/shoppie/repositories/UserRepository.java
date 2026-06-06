fcd00505e0662aa41ae94aad485acb26e1081d78
Sanan Tahmazli <sanan.tahmazli2329@gmail.com>
2026-06-06 17:13:29 +0400

    Created UserRepository

---
+++ src/main/java/com/shoppie/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.shoppie.repositories;
+
+import com.shoppie.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmailContainsIgnoreCase(String email);
+}
